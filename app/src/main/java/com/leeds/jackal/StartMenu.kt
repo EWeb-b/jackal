b0b05125c89e17f5a08ef09c871282d148f96001
Khalil Ibrahim <khalil@beytna.net>
2020-05-12 19:45:10 +0100

    Start menu redirects work as intended

--- app/src/main/java/com/leeds/jackal/StartMenu.kt
+++ app/src/main/java/com/leeds/jackal/StartMenu.kt
@@ -1,12 +1,29 @@
 package com.leeds.jackal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class StartMenu : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_menu)
+        val goToLoginActivity =
+            findViewById<View>(R.id.goToLoginActivity) as Button
+        goToLoginActivity.setOnClickListener {
+            val startLoginIntent =
+                Intent(applicationContext, Login::class.java)
+            startActivity(startLoginIntent)
+        }
+        val goToRegisterActivity =
+            findViewById<View>(R.id.goToRegisterActivity) as Button
+        goToRegisterActivity.setOnClickListener {
+            val startRegisterIntent =
+                Intent(applicationContext, Register::class.java)
+            startActivity(startRegisterIntent)
+        }
     }
 }
