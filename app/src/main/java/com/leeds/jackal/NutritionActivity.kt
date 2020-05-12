41bb4c38908c0f728da17cbb863af6c994c8d38c
EWebb <sc16eahw@leeds.ac.uk>
2020-05-12 18:02:59 +0100

    created base structure for the nutrition api activity and response interface

--- app/src/main/java/com/leeds/jackal/NutritionActivity.kt
+++ app/src/main/java/com/leeds/jackal/NutritionActivity.kt
@@ -1,19 +1,12 @@
 package com.leeds.jackal
 
-import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import android.os.Bundle
 
-class NutritionActivity: AppCompatActivity() {
+class NutritionActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nutrition)
+    }
 }
