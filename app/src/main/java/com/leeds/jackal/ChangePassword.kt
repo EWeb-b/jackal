5449a700b46d540f28e1cd73a3e974da04881aa5
Khalil Ibrahim <khalil@beytna.net>
2020-05-17 13:10:36 +0100

    change password activity added

---
+++ app/src/main/java/com/leeds/jackal/ChangePassword.kt
@@ -0,0 +1,12 @@
+package com.leeds.jackal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangePassword : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
