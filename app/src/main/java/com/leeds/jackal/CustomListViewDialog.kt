b65daf50b76d9fece0291ac1ad7dbe64ceb8e708
Jack Boreham <sc16jeb@leeds.ac.uk>
2020-05-17 19:06:21 +0100

    Text detection feature finished

---
+++ app/src/main/java/com/leeds/jackal/CustomListViewDialog.kt
@@ -0,0 +1,59 @@
+package com.leeds.jackal
+
+import android.app.Activity
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.layout_recipe_dialog.*
+
+
+class CustomListViewDialog(var activity: Activity, internal var adapter: RecyclerView.Adapter<*>, var ingredients:ArrayList<String>) : Dialog(activity),
+    View.OnClickListener {
+    var dialog: Dialog? = null
+
+    val user = FirebaseAuth.getInstance().currentUser
+    internal var recyclerView: RecyclerView? = null
+    private var mLayoutManager: RecyclerView.LayoutManager? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.layout_recipe_dialog)
+
+        recyclerView = findViewById(R.id.recycler_view) as RecyclerView//recycler_view
+        recyclerView?.layoutManager = LinearLayoutManager(activity,RecyclerView.VERTICAL,false)
+        //recyclerView?.layoutManager = mLayoutManager
+        recyclerView?.adapter = adapter
+
+        dialog_save_button.setOnClickListener(this)
+        dialog_cancel_button.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.dialog_save_button -> {
+                val recipeName = edit_recipename.text.toString()
+                val serves = edit_serves.text.toString()
+                val recipe = Recipe(recipeName,serves.toInt(),ingredients)
+                // Write a message to the database
+                val database = Firebase.database
+                val myRef = database.getReference().child("users").child(user!!.uid).child("recipes").push()
+
+                myRef.setValue(recipe)
+                dismiss()
+            }
+            R.id.dialog_cancel_button -> dismiss()
+            else -> {
+            }
+        }//Do Something
+        dismiss()
+    }
+}
--- app/src/main/java/com/leeds/jackal/PhotoRecipeActivity.kt
+++ app/src/main/java/com/leeds/jackal/PhotoRecipeActivity.kt
@@ -1,65 +1,188 @@
 package com.leeds.jackal
 
+
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
-
+import kotlinx.android.synthetic.main.activity_photo_recipe.*
 import java.io.File
 import java.io.IOException
 import java.util.*
+import kotlin.collections.ArrayList
 
 class PhotoRecipeActivity : AppCompatActivity() {
 
 
 
+
     lateinit var textBlock :TextView
     lateinit var image : ImageView
+    lateinit var galleryButton: ImageButton
+    lateinit var photoButton: Button
+    private var ingredients: ArrayList<String> = ArrayList()
+
+    internal var customDialog: CustomListViewDialog? = null
+
+    val WRITE_STORAGE = 100
+    val READ_STORAGE = 101
+    val SELECT_PHOTO = 102
+    val REQUEST_TAKE_PHOTO = 2
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_recipe)
 
-
-        val photoButton = findViewById<Button>(R.id.camera_button)
-        textBlock = findViewById<TextView>(R.id.photoText)
+        //Initialise and inflate activity views
+        photoButton = findViewById(R.id.camera_button)
+        textBlock = findViewById(R.id.photoText)
         image =  findViewById(R.id.testPhoto)
+        galleryButton = findViewById(R.id.gallery_button)
+
+        setupPermissions()
 
         photoButton.setOnClickListener {
             dispatchTakePictureIntent()
 
         }
 
+        setUpNewImageListener()
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK){
-            val imgFile = File(currentPhotoPath)
-            if (imgFile.exists()){
-                val uri = Uri.fromFile(imgFile)
-                image.setImageURI(uri)
-                runTextRecognition(getImgFromPath(this@PhotoRecipeActivity,uri))
-            }
-            image.setImageBitmap(BitmapFactory.decodeFile(currentPhotoPath))
+    private fun setupPermissions(){
+        val permissionRead = ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        val permissionWrite = ContextCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
+        if (permissionRead!= PackageManager.PERMISSION_GRANTED){
+            Log.i("Access Error:","Permission to access media denied")
+            makeRequestRead()
+        }
+        if (permissionWrite!= PackageManager.PERMISSION_GRANTED){
+            Log.i("Access Error:","Permission to access media denied")
+            makeRequestWrite()
+        }
+
+    }
+    private fun makeRequestWrite(){
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            WRITE_STORAGE )
+    }
+    private fun makeRequestRead(){
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+            READ_STORAGE )
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            READ_STORAGE ->{
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Log.i("Access Error:", "Permission denied by user")
+                    makeRequestRead();
+                }else{
+                    Log.i("Access Granted:", "Permission granted by user")
+
+                }
+            }
+            WRITE_STORAGE ->{
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Log.i("Access Error:", "Permission denied by user")
+                    makeRequestWrite();
+                }else{
+                    Log.i("Access Granted:", "Permission granted by user")
+
+                }
+            }
         }
     }
 
-    val REQUEST_TAKE_PHOTO = 1
 
-    //Use the existing camera app on the phone to
+    fun checkPermission(requestCode: Int) {
+        when (requestCode) {
+            WRITE_STORAGE -> {
+                val hasWriteExternalStoragePermission = ActivityCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+
+//If we have access to external storage...//
+                if (hasWriteExternalStoragePermission == PackageManager.PERMISSION_GRANTED) {
+
+//...call selectPicture, which launches an Activity where the user can select an image//
+                    //selectPicture()
+
+//If permission hasn’t been granted, then...//
+                } else {
+
+//...request the permission//
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        requestCode
+                    )
+                }
+            }
+        }
+    }
+
+
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, returnedIntent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, returnedIntent)
+        when (requestCode) {
+            SELECT_PHOTO -> if (resultCode == Activity.RESULT_OK) {
+                returnedIntent?.data?.let {
+                    val selectedImageBitmap = resizeImage(it)
+                    testPhoto.setImageBitmap(selectedImageBitmap)
+                    //Log.d("LIst SIZE0", ingredients.size.toString())
+                    val firebaseImg = FirebaseVisionImage.fromBitmap(selectedImageBitmap!!)
+                    runTextRecognition(firebaseImg)
+                }
+            }
+            REQUEST_TAKE_PHOTO -> if (resultCode == Activity.RESULT_OK) {
+
+                val imgFile = File(currentPhotoPath)
+                if (imgFile.exists()) {
+                    galleryAddPic()
+                    val uri = Uri.fromFile(imgFile)
+                    image.setImageURI(uri)
+                    runTextRecognition(getImgFromPath(this@PhotoRecipeActivity, uri))
+                }
+                image.setImageBitmap(BitmapFactory.decodeFile(currentPhotoPath))
+            }
+        }
+
+
+    }
+
+
+    //Use the existing camera app on the phone to capture an Image
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             // Ensure that there's a camera activity to handle the intent
@@ -79,10 +202,6 @@ class PhotoRecipeActivity : AppCompatActivity() {
                         it
                     )
 
-//                    val path = photoURI.path
-//                    var textView= findViewById<TextView>(R.id.photoText)
-//                    textView.text = path
-//                    textView.visibility = View.VISIBLE
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
                 }
@@ -99,8 +218,9 @@ class PhotoRecipeActivity : AppCompatActivity() {
         // Create an image file name
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        Log.d("Storage Location:", storageDir!!.absolutePath)
         return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
+            "RECIPEZ_${timeStamp}_", /* prefix */
             ".jpg", /* suffix */
             storageDir /* directory */
         ).apply {
@@ -120,6 +240,9 @@ class PhotoRecipeActivity : AppCompatActivity() {
         return null
     }
 
+    private fun getBitmapFromUri(filePath: Uri): Bitmap? {
+        return MediaStore.Images.Media.getBitmap(this.contentResolver, filePath)
+    }
 
     //Run initial text recognition on the photo
     private fun runTextRecognition(image : FirebaseVisionImage?){
@@ -128,23 +251,130 @@ class PhotoRecipeActivity : AppCompatActivity() {
             val result = detector.processImage(image)
                 .addOnSuccessListener { firebaseVisionText ->
 
-                    textBlock.setText( firebaseVisionText.text)
+                    //Process text
+
+                    //Prompt dialog for user
+                    //Dialog start
+                    Log.d("Picture Text", firebaseVisionText.text)
+
                     textBlock.visibility = View.VISIBLE
-//                    for (block in firebaseVisionText.textBlocks){
-//                        val boundingBox = block.boundingBox
-//                        val text = block.text
-//
-//                        for (line in block.lines){
-//
-//                        }
-//                    }
+                    Log.d("Text blocks", firebaseVisionText.textBlocks.size.toString())
+                    for (block in firebaseVisionText.textBlocks) {
+                        for (line in block.lines){
+                            Log.d("LINE TEXT", line.text)
+                            ingredients.add(line.text)
+                        }
+                    }
+                    Log.d("LIst SIZE2", ingredients.size.toString())
+                    val dataAdapter = RecyclerAdapter(ingredients)
+                    for (item in ingredients) {
+                        Log.i("LIST CONTENTS: " , item)
+                    }
+                    customDialog = CustomListViewDialog(this@PhotoRecipeActivity, dataAdapter,ingredients)
+                    customDialog!!.show()
+                    customDialog!!.setCanceledOnTouchOutside(false)
+                    Log.d("List SIZE", ingredients.size.toString())
                 }
                 .addOnFailureListener { e ->
                     // Task failed with an exception
                     e.printStackTrace()
                 }
 
+
         }
 
     }
-}
+
+    private fun resizeImage(selectedImage: Uri): Bitmap? {
+
+        return getBitmapFromUri(selectedImage)?.let {
+            val scaleFactor = Math.max(
+                it.width.toFloat() / testPhoto.width.toFloat(),
+                it.height.toFloat() / testPhoto.height.toFloat()
+            )
+
+            Bitmap.createScaledBitmap(
+                it,
+                (it.width / scaleFactor).toInt(),
+                (it.height / scaleFactor).toInt(),
+                true
+            )
+        }
+    }
+
+    private fun setUpNewImageListener() {
+        galleryButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, SELECT_PHOTO)
+        }
+    }
+
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
+
+
+
+//
+//        fun createTempFile(file: File?): File {
+//            var file = file
+//            val directory = File(
+//                Environment.getExternalStorageDirectory()
+//                    .path + "/com.jessicathornsby.myapplication"
+//            )
+//            if (!directory.exists() || !directory.isDirectory) {
+//                directory.mkdirs()
+//            }
+//            if (file == null) {
+//                file = File(directory, "orig.jpg")
+//            }
+//            return file
+//        }
+//
+//        private fun compressPhoto(photoFile: File, bitmap: Bitmap?): Bitmap? {
+//            try {
+//                val fOutput = FileOutputStream(photoFile)
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, fOutput)
+//                fOutput.close()
+//            } catch (exception: IOException) {
+//                exception.printStackTrace()
+//            }
+//            return bitmap
+//        }
+//    }
+//    private static Bitmap rotateImageIfRequired(Bitmap img, Uri selectedImage) throws IOException {
+//
+//        ExifInterface ei = new ExifInterface(selectedImage.getPath());
+//        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+//
+//        switch (orientation) {
+//            case ExifInterface.ORIENTATION_ROTATE_90:
+//            return rotateImage(img, 90);
+//            case ExifInterface.ORIENTATION_ROTATE_180:
+//            return rotateImage(img, 180);
+//            case ExifInterface.ORIENTATION_ROTATE_270:
+//            return rotateImage(img, 270);
+//            default:
+//            return img;
+//        }
+//    }
+//
+//    private static Bitmap rotateImage(Bitmap img, int degree) {
+//        Matrix matrix = new Matrix();
+//        matrix.postRotate(degree);
+//        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+//        img.recycle();
+//        return rotatedImg;
+//    }
+
+
+
+
+
+
+    }
